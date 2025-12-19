88c49b2157899134d21c0ba04f7ffb6ec6286cd1
Jorge <jorge.medi.08@gmail.com>
2025-12-18 20:25:53 -0600

    Api CRUD

---
+++ WebAPICRUD/Models/DbcrudContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPICRUD.Models;
+
+public partial class DbcrudContext : DbContext
+{
+    public DbcrudContext()
+    {
+    }
+
+    public DbcrudContext(DbContextOptions<DbcrudContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Empleado> Empleados { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Empleado>(entity =>
+        {
+            entity.HasKey(e => e.IdEmpleado).HasName("empleado_pkey");
+
+            entity.ToTable("empleado");
+
+            entity.Property(e => e.IdEmpleado).HasColumnName("id_empleado");
+            entity.Property(e => e.Correo)
+                .HasMaxLength(50)
+                .HasColumnName("correo");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .HasColumnName("nombre");
+            entity.Property(e => e.Sueldo).HasColumnName("sueldo");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ WebAPICRUD/Program.cs
@@ -0,0 +1,47 @@
+using WebAPICRUD.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DbcrudContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("CadenaSQL") ?? throw new InvalidOperationException("Connection string 'DbcrudContext' not found.")));
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("NuevaPolitica",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("NuevaPolitica");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
